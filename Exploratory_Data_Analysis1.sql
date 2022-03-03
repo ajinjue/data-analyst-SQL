e216f6261ace8c80d826d42173de07b2c67de5a0
unknown <capurishi@gmail.com>
2022-03-03 16:24:21 +0100

    Added new file and rename existing file

---
+++ Exploratory_Data_Analysis1.sql
@@ -0,0 +1,32 @@
+/* 
+Query #1: Extracting tables and their columns from sakila schema
+*/
+SELECT 
+	table_name AS tables,
+    -- concatenate all the columns of a table
+    GROUP_CONCAT(DISTINCT column_name SEPARATOR ',  ') AS column_names
+FROM information_schema.columns
+WHERE table_schema = 'sakila'
+GROUP BY table_name;
+
+/*
+Query #2: Store the result of above as a virtual table (VIEW)
+*/
+CREATE VIEW table_list AS
+SELECT 
+	table_name AS tables,
+    -- concatenate all the columns of a table
+    GROUP_CONCAT(DISTINCT column_name SEPARATOR ',  ') AS column_names
+FROM information_schema.columns
+WHERE table_schema = 'sakila'
+GROUP BY table_name;
+
+/* 
+Query #3: List the data type of the columns in each table
+*/
+SELECT 
+	table_name,
+	column_name,
+    data_type
+FROM information_schema.columns
+WHERE table_schema = 'sakila'
