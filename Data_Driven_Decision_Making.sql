a97a3c941ce78f298e2d3f11b0df35c871ef1c07
unknown <capurishi@gmail.com>
2022-03-09 13:51:36 +0100

    Added a new file

---
+++ Data_Driven_Decision_Making.sql
@@ -0,0 +1,8 @@
+/* Objectives of Data Driven Decision making
+	Short term: Information for operational decisions e.g
+		- Popularity of actors to decide which movies to invest in.
+        - Revenue of the last months to estimate budget for short term investments.
+	Long term: Information for strategic decisions or planning e.g
+		- Success across countries to decide on market extensions.
+        - Development of revenue for long term investments.
+*/
